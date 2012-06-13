788e14c3d046ca5ca260788974e52ce948c3885b
Dan Leyden <daniel_leyden@dell.com>
2012-06-13 13:11:49 +0100

    Adding some docs for new params on client class

--- manifests/client.pp
+++ manifests/client.pp
@@ -7,9 +7,33 @@
 #     default unspecified
 #     this is the name of the cluster
 #
+#   $multicast_address
+#     default 239.2.11.71
+#     this is the multicast address to publish metrics on
+#
+#   $owner
+#     default unspecified
+#     the owner of the cluster
+#
+#   $send_metadata_interval
+#     default 0
+#     the interval (seconds) at which to resend metric metadata.
+#     If running unicast, this should not be 0
+#
 #   $udp_port
 #     default 8649
 #     this is the udp port to multicast metrics on
+#
+#   $unicast_listen_port
+#     default 8649
+#     the port to listen for unicast metrics on
+#
+#   $unicast_targets
+#     default []
+#     an array of servers to send unicast metrics to.
+#     each entry in the array should be a hash containing an entry named
+#     ipaddress and an entry named port
+#     e.g. [ {'ipaddress' => '1.2.3.4', 'port' => '1234'} ]
 #
 # Actions:
 #   installs the ganglia client
@@ -26,14 +50,18 @@
 #   class {'ganglia::client':
 #     cluster  => 'mycluster',
 #     udp_port => '1234',
-#     owner => 'myconpany',
+#     owner    => 'mycompany',
 #   }
 #
-class ganglia::client ($cluster='unspecified', $udp_port='8649', $owner='unspecified',
-  $multicast_address = "239.2.11.71",
-  $unicast_listen_port = "8649",
-  $unicast_targets = [],
-  $send_metadata_interval = 0, ) {
+class ganglia::client (
+  $cluster='unspecified',
+  $multicast_address = '239.2.11.71',
+  $owner='unspecified',
+  $send_metadata_interval = 0,
+  $udp_port='8649',
+  $unicast_listen_port = '8649',
+  $unicast_targets = []
+  ) {
 
   case $operatingsystem {
     'Ubuntu': {$ganglia_client_pkg = 'ganglia-monitor'}
